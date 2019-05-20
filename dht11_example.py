b73c5a1c552666fe812fa3404f8abbbeb4a58dc9
suvrodeepta <41569342+suvrodeepta@users.noreply.github.com>
2019-05-20 22:46:05 +0530

    Update for adafruit cloud implementation

--- dht11_example.py
+++ dht11_example.py
@@ -2,6 +2,10 @@ import RPi.GPIO as GPIO
 import dht11
 import time
 
+# import libraries 
+from Adafruit_IO import *
+
+aio = Client('','') #username and key respectively 
 # initialize GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -14,5 +18,6 @@ while True:
     result = instance.read()
     if result.is_valid():
         print("Temp: %d C" % result.temperature +' '+"Humid: %d %%" % result.humidity)
+        aio.send("#dashboard name", Temp: %d C" % result.temperature +' '+"Humid: %d %%" % result.humidity)
 
     time.sleep(1)
